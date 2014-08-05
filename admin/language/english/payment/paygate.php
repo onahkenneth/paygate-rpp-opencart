7ff5c958607e5a5517ee59326f6619d72dc8e9b1
Kenneth Onah <onahkenneth@users.noreply.github.com>
2014-08-05 19:25:39 +0200

    Create and update paygate.php
    add language text to paygate language file

---
+++ admin/language/english/payment/paygate.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ *
+ * Paygate Opencart Plugin
+ *
+ * @author Kenneth Onah
+ * @author info@collmaxtech.co.za
+ * @version 1.0.0
+ * @package Opencart
+ * @subpackage payment
+ * @copyright Copyright (C) 2013 Collmax Technologies - All rights reserved.
+ */
+
+// Heading
+$_['heading_title']				= 'PayGate';
+
+// Text
+$_['text_payment']				= 'Payment';
+$_['text_success']				= 'Success: You have modified PayGate account details.';
+$_['text_paygate']			 	= '<a onclick="window.open(\'https://www.paygate.co.za\');"><img src="view/image/payment/paygate.jpg" alt="Paygate_logo" title="Paygate" style="border: 1px solid #EEEEEE;" /></a>';
+$_['text_authorization']		= 'Authorization';
+$_['text_sale']					= 'Sale';
+
+// Entry
+$_['entry_merchant_id']			= 'PayGate Merchant Id:';
+$_['entry_secret_key']			= 'PayGate Secret Key:';
+$_['entry_sort_order']			= 'Sort Order:';
+$_['entry_status']				= 'Status:';
+$_['entry_completed_status']  	= 'Completed Status:';
+$_['entry_denied_status']  		= 'Denied Status:';
+$_['entry_failed_status']		= 'Failed Status:';
+
+// Error
+$_['error_permission']			= 'Warning: You do not have permission to modify payment PayGate';
+$_['error_merchant_id']			= 'PayGate merchant id is required';
+$_['error_secret_key']			= 'PayGate secret key is required';
+?>
