bab92191faf984972003323c7e884c8d681cc7f1
Kenneth Onah <onahkenneth@users.noreply.github.com>
2014-08-05 20:27:58 +0200

    Create paygate.php

---
+++ catalog/language/english/payment/paygate.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *
+ * PayGate Opencart Plugin
+ *
+ * @author Collmax Technologies
+ * @author info@collmaxtech.co.za
+ * @version 1.0.0
+ * @package Opencart
+ * @subpackage payment
+ * @copyright Copyright (C) 2013 Collmax Technologies  - All rights reserved.
+ */
+ 
+// Heading
+$_['heading_title']     = 'Thank you for shopping with %s .... ';
+
+// Text
+$_['text_title'] 		= 'PayGate (Pty) Limited';
+$_['text_response']     = 'Response from PayGate:';
+$_['text_success']      = '... your payment was successfully received.';
+$_['text_success_wait'] = '<b><span style="color: #FF0000">Please wait...</span></b> whilst we finish processing your order.<br>If you are not automatically re-directed in 10 seconds, please click <a href="%s">here</a>.';
+$_['text_failure']      = '... Your payment was unsuccessful!';
+$_['text_failure_wait'] = '<b><span style="color: #FF0000">Please wait...</span></b><br>If you are not automatically re-directed in 10 seconds, please click <a href="%s">here</a>.';										  
+$_['text_total'] 		= 'Shipping, Handling, Discounts & Taxes';
+?>
